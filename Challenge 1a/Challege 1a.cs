180feffcb4694616797e95df45182d7d71f79000
Nate Martin <captainleroy175@gmail.com>
2023-09-07 20:16:29 -0600

    Submission for Challenge 1a. I hope this works as i have struggled with the code/

---
+++ Challenge 1a/Challege 1a.cs
@@ -0,0 +1,38 @@
+using System;
+
+	challenge1a Program
+	{
+		string statement = "Does Nate like Weekends?"
+		int percentage = 100;
+		bool statement = true;
+		
+	}
+		{
+			Consosle.WriteLine(statement);
+
+			Console.WriteLine(bool);
+
+			Console.writeLine(percentage):
+
+		}
+	{
+		int numberone = 75;
+		int numbertwo = 25;
+		
+	}
+		{
+			Consosle.WriteLine(numberone + numbertwo);
+
+			Console.WriteLine(numbertwo - numberone);
+
+		}
+
+	{
+		Console.WriteLine("What's your favorite number");
+
+		int favorite = Console.ReadLine();
+		
+		Console.WriteLine("So your favoirite number is " + favorite ". (Mine is 5)"; 	
+	}
+
+	{
